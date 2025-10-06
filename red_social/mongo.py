d84c6c0148ef9130ffe0879fc10f503fa5e2e3bd
AlexandroDlv <ar5140437@gmail.com>
2025-10-06 14:52:27 -0600

    .

--- red_social/mongo.py
+++ red_social/mongo.py
@@ -1,8 +1,12 @@
 import os
 from pymongo import MongoClient
+from dotenv import load_dotenv
+
+# Cargar variables del archivo .env
+load_dotenv()
 
 MONGODB_URI = os.getenv("MONGODB_URI")
-MONGO_DB_NAME = os.getenv("MONGO_DB_NAME", "socialcaz")  # tu base de datos
+MONGO_DB_NAME = os.getenv("MONGO_DB_NAME", "socialcaz")  # nombre de tu base de datos
 
 if not MONGODB_URI:
     raise RuntimeError("MONGODB_URI no está configurado en variables de entorno")
@@ -11,6 +15,6 @@ _client = MongoClient(MONGODB_URI)
 db = _client[MONGO_DB_NAME]
 
 # Colecciones usadas
-users_coll = db["usuarios"]  # tu colección de usuarios
+users_coll = db["usuarios"]
 posts_coll = db["posts"]
 audit_coll = db["audit_logs"]
